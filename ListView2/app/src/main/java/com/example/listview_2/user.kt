45accb75ad3523122337ae080c5a4ca165fe3c8b
Bhavesh Charde <115037516+BhaveshC07@users.noreply.github.com>
2024-09-03 14:29:11 +0530

    Delete ListView2 directory

--- ListView2/app/src/main/java/com/example/listview_2/user.kt
+++
@@ -1,7 +0,0 @@
-package com.example.listview_2
-
-data class user(var name: String,
-                var lastmsg: String,
-                var lastseen:String,
-                var phone:String,
-                var image:Int )
